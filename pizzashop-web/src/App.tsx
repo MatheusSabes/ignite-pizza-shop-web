c1aad9552c572f706c84b9b4cfeff1bd01c72988
MatheusSabes <msabes@outlook.com>
2024-02-15 22:39:58 -0300

    starting a pizza shop project

---
+++ pizzashop-web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+import "./global.css";
+
+export function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <div className="flex justify-center items-center h-screen">
+        <Button>Enviar</Button>
+      </div>
+    </>
+  );
+}
